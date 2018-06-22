b24335556861b31db0c37d4c3084e74dc367529b
Mustafa Musa <31841917+stopamusa@users.noreply.github.com>
2018-06-22 23:15:37 +0800

    21. Instance Update View

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from .models import Post
@@ -14,18 +14,14 @@ def post_create(request):
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
 		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
 
-	# if request.method == "POST":
-	# 	print request.POST.get("content")
-	# 	print request.POST.get("title")
 	context = {
 		"form": form,
 	}
 	return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
-	# return HttpResponse("<h1>detail</h1>")
-	# instance = Post.objects.get(id=1)
 	instance = get_object_or_404(Post, id=id)
 	context = {
 		"title": instance.title,
@@ -41,19 +37,22 @@ def post_list(request): #list items
 		"title": "List"
 	}
 	return render(request, "index.html", context)
-	# if request.user.is_authenticated():
-	# 	context = {
-	# 		"title": "My User List"
-	# 	}
-	# else:
-	# 	context = {
-	# 	"title": "List"
-	# 	}
 	
-	# return HttpResponse("<h1>list</h1>")
 
-def post_update(request):
-	return HttpResponse("<h1>update</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
+
 
 def post_delete(request):
 	return HttpResponse("<h1>delete</h1>")
