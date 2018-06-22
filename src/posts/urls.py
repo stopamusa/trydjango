c4801ab8798148de0005e9eb60e3938bcee6df42
Mustafa Musa <31841917+stopamusa@users.noreply.github.com>
2018-06-22 09:27:33 +0800

    first mus commit

---
+++ src/posts/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+# from django.contrib import admin
+# from posts.views import post_home
+# from posts import views
+from . import views
+
+urlpatterns = [
+    # url(r'^posts/$', views.post_home),
+    # url(r'^$', post_home),
+    url(r'^$', views.post_list),
+    url(r'^create/$', views.post_create),
+    url(r'^detail/$', views.post_detail),
+    url(r'^update/$', views.post_update),
+    url(r'^delete/$', views.post_delete),
+    
+]
---
+++ src/posts/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
+
+def post_detail(request):
+	return HttpResponse("<h1>detail</h1>")
+
+def post_list(request):
+	return HttpResponse("<h1>list</h1>")
+
+def post_update(request):
+	return HttpResponse("<h1>update</h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1>delete</h1>")
