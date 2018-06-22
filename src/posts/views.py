211402937a5146e75938d7171ab5dd91ec34c1bd
Mustafa Musa <31841917+stopamusa@users.noreply.github.com>
2018-06-22 10:01:24 +0800

    15 template context

--- src/posts/views.py
+++ src/posts/views.py
@@ -10,10 +10,26 @@ def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
-	return HttpResponse("<h1>detail</h1>")
+	# return HttpResponse("<h1>detail</h1>")
+	context = {
+		"title": "Detail"
+	}
+	return render(request, "index.html", context)
 
-def post_list(request):
-	return HttpResponse("<h1>list</h1>")
+def post_list(request): #list items
+	# if request.user.is_authenticated():
+	# 	context = {
+	# 		"title": "My User List"
+	# 	}
+	# else:
+	# 	context = {
+	# 	"title": "List"
+	# 	}
+	context = {
+	"title": "List"
+	}
+	return render(request, "index.html", context)
+	# return HttpResponse("<h1>list</h1>")
 
 def post_update(request):
 	return HttpResponse("<h1>update</h1>")
