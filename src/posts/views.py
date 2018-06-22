3d46cb3db5105462e9a81cafb70e8f715866ab06
Mustafa Musa <31841917+stopamusa@users.noreply.github.com>
2018-06-23 00:05:15 +0800

    23. Delete View

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,7 +2,7 @@
 
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
 
@@ -59,5 +59,8 @@ def post_update(request, id=None):
 	return render(request, "post_form.html", context)
 
 
-def post_delete(request):
-	return HttpResponse("<h1>delete</h1>")
+def post_delete(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully Deleted")
+		return redirect("posts:list")
