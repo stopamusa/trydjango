6544a88649d9bbf74d86097f51950d5ff9453318
Mustafa Musa <31841917+stopamusa@users.noreply.github.com>
2018-06-24 00:41:52 +0800

    33. Associate User to Post with a Foreign Key

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
@@ -16,6 +17,7 @@ def upload_location(instance, filename):
 	return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	title = models.CharField(max_length=120)
 	slug = models.SlugField(unique=True)
 	image = models.ImageField(upload_to=upload_location, 
--- src/posts/views.py
+++ src/posts/views.py
@@ -15,6 +15,8 @@ from .models import Post
 def post_create(request):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
+
+
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -66,10 +68,13 @@ def post_list(request): #list items
 def post_update(request, slug=None):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
+
+
 	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
+		instance.user = request.user
 		instance.save()
 		messages.success(request, "Successfully Saved")
 		return HttpResponseRedirect(instance.get_absolute_url())
